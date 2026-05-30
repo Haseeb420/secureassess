14b921b3b835fe48e26847cdcf915d8b1ea9e872
Muhammad Haseeb Malik <muhammadhaseebtcf@gmail.com>
2026-05-30 13:48:10 +0500

    feat(config): add brand color palette and CSS variables

--- packages/config/tailwind.config.base.js
+++ packages/config/tailwind.config.base.js
@@ -5,7 +5,20 @@ module.exports = {
     "../../packages/ui/src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          navy:           '#2A2A47',
+          'navy-light':   '#3A3A60',
+          'navy-pale':    '#EEEEF5',
+          'navy-dark':    '#1A1A30',
+          'navy-mid':     '#22223A',
+          orange:         '#DE5E1F',
+          'orange-light': '#F06B28',
+          'orange-pale':  '#FFF0E8',
+        },
+      },
+    },
   },
   plugins: [],
 };
